5169cb1726f24d84cf00d4169a8ab69999b6ce19
Masood Khan <kmasood707@gmail.com>
2022-07-17 17:36:40 +0530

    post api

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostPublishRequest;
+use App\Models\Website;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function publish($websiteSlug, PostPublishRequest $request)
+    {
+        $website = Website::query()
+            ->whereSlug($websiteSlug)
+            ->firstOrFail();
+
+        $post = $website->posts()->create([
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'description' => $request->description,
+        ]);
+
+        // Generate Post Published Event
+        event()->
+
+        return response()->json([
+            'message' => 'Post published',
+            'data' => compact('post'),
+        ]);
+    }
 }
---
+++ app/Http/Requests/PostPublishRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+
+class PostPublishRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'slug' => Str::slug($this->title),
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'string'], 
+            'slug' => ['required', 'unique:posts'], 
+            'description' => ['required', 'string'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'slug.unique' => 'It seems to be duplicate post!',
+        ];
+    }
+
+}
