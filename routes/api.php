a591a9d2609847647f3b586c32fdf5ca7f27a33a
Masood Khan <kmasood707@gmail.com>
2022-07-17 16:05:27 +0530

    Factory and Seeder setup

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('posts/{website}', [PostController::class, 'store'])->name('posts');
+
+Route::post('subscribe/{website}', [SubscriptionController::class, 'store'])->name('subscribe');
